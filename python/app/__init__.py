00845acf71618e4f3c060c5d1ddbc9d60d2e8063
Kévin B <kevinbillerach@yahoo.fr>
2023-09-08 09:57:11 +0200

    prometheus

--- python/app/__init__.py
+++ python/app/__init__.py
@@ -5,8 +5,10 @@ from decouple import config
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
 app = Flask(__name__)
+metrics = GunicornPrometheusMetrics(app)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
